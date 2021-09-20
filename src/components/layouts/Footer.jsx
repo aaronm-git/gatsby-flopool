df537ba5928bcd795e943d01ec240d01c1808cfd
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2021-09-20 12:49:29 -0400

    updated footer style

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import "../../styles/footer.css";
+import { EnvelopeFill, TelephoneFill } from "react-bootstrap-icons";
 
 const Footer = () => {
   return (
@@ -18,11 +19,28 @@ const Footer = () => {
                   placeholder="tracedSVG"
                 />
               </Link>
-              <ul className="mt-2 border-top border-warning py-2">
+              <ul className="mt-2 border-top border-warning pt-2">
                 <li>Florida Swimming Pools LLC.</li>
                 <li>13301 SW 97th St.</li>
                 <li>Suite #118</li>
                 <li>Miami, Florida 33186</li>
+              </ul>
+              <ul className="mt-2 border-top border-warning py-2">
+                <h5>Miami, FL. Office</h5>
+                <li>Mon-Sat: 8:30 AM – 6 PM</li>
+                <li>Sunday: 10 AM – 4 PM</li>
+                <li>
+                  <a href="tel:3052537665">
+                    <TelephoneFill />
+                    (305) 253-7665
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto:cs@flopool.com">
+                    <EnvelopeFill />
+                    cs@flopool.com
+                  </a>
+                </li>
               </ul>
             </Col>
             <Col md={3}>
@@ -49,6 +67,9 @@ const Footer = () => {
                   <Link to="/get-a-quote">Get a quote</Link>
                 </li>
                 <li>
+                  <Link to="/make-a-payment">Make a Payment</Link>
+                </li>
+                <li>
                   <Link to="/jobs">We're Hiring!</Link>
                 </li>
               </ul>
@@ -57,22 +78,23 @@ const Footer = () => {
               <h5 className="border-bottom border-warning pb-2">Services</h5>
               <ul className="ul-links">
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/services/residential">
+                    Residential Pool Service
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link to="/services/commercial">Commercial Pool Service</Link>
                 </li>
                 <li>
-                  <Link to="/services">Services</Link>
+                  <Link to="/services/repair">Pool Equipment Repair</Link>
                 </li>
                 <li>
-                  <Link to="/contact-us">Contact Us</Link>
+                  <Link to="/services/leak-detection">Leak Detection</Link>
                 </li>
                 <li>
-                  <Link to="/blog">Blog</Link>
-                </li>
-                <li>
-                  <Link to="/get-a-quote">Get a quote</Link>
+                  <Link to="/services/remodeling">
+                    Pool and Deck Remodeling
+                  </Link>
                 </li>
               </ul>
             </Col>{" "}
@@ -80,12 +102,14 @@ const Footer = () => {
         </Container>
       </section>
       <section className="bg-primary py-2 text-center">
-        <span className="d-block">FloPool is fully licensed and insured.</span>
-        <span className="d-block">
+        <small className="d-block">
+          FloPool is fully licensed and insured.
+        </small>
+        <small className="d-block">
           Master Swimming Pool Maintenance Unlimited Certified Contractor for
           Mismi Dade and Florida State
-        </span>
-        <span className="d-block">CC 08P000918 / RP 252555135</span>
+        </small>
+        <small className="d-block">CC 08P000918 / RP 252555135</small>
       </section>
       <section
         className="text-center py-2"
