aa631a5f6c282c5876ec83dc2e7a0d4bf472954f
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2021-09-23 18:08:19 -0400

    404 page style updated

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 import Layout from "../components/layouts/Main";
-
+import { Link } from "gatsby";
+import { ChevronLeft } from "react-bootstrap-icons";
 // markup
 const NotFoundPage = () => {
   return (
     <Layout>
-      <h1>404</h1>
-      <p>These are not the URL you're looking for.</p>
+      <section
+        className="subsection text-center"
+        style={{ minHeight: "400px" }}
+      >
+        <h1 className="display-1">404</h1>
+        <p>Looks like that page does not exist.</p>
+        <Link to="/" className="btn-link">
+          <ChevronLeft />
+          Return to the homepage
+        </Link>
+      </section>
     </Layout>
   );
 };
