c9ec785afcf9bb5edaf953c62d9cc70b8a95b240
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2021-10-04 00:16:11 -0400

    made certain elements links

--- src/components/Home/KeyBenefits.jsx
+++ src/components/Home/KeyBenefits.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 const KeyBenefits = () => {
   return (
@@ -11,16 +12,20 @@ const KeyBenefits = () => {
         </h1>
         <Row>
           <Col md={4}>
-            <StaticImage
-              src="../../images/home/key-benefits.png"
-              alt="Weekly residential and commercial pool service in Miami, Florida"
-              placeholder="tracedSVG"
-              height={250}
-            />
+            <Link to="/services/residential">
+              <StaticImage
+                src="../../images/home/key-benefits.png"
+                alt="Weekly residential and commercial pool service in Miami, Florida"
+                placeholder="tracedSVG"
+                height={250}
+              />
+            </Link>
             <div className="copy">
-              <h2 className="subtitle text-primary">
-                Pool Cleaning & Maintenance
-              </h2>
+              <Link to="/services/residential" className="text-decoration-none">
+                <h2 className="subtitle text-primary">
+                  Pool Cleaning & Maintenance
+                </h2>
+              </Link>
               <p>
                 South Florida residents, condos, and homeowner associations have
                 many options when it comes to choosing a pool service company in
@@ -32,16 +37,20 @@ const KeyBenefits = () => {
             </div>
           </Col>
           <Col md={4}>
-            <StaticImage
-              src="../../images/home/key-benefits-3.png"
-              alt="We install, service and repair all brand-name pool and spa equipment in Miami, Florida"
-              placeholder="tracedSVG"
-              height={250}
-            />
+            <Link to="/services/repairs">
+              <StaticImage
+                src="../../images/home/key-benefits-3.png"
+                alt="We install, service and repair all brand-name pool and spa equipment in Miami, Florida"
+                placeholder="tracedSVG"
+                height={250}
+              />
+            </Link>
             <div className="copy">
-              <h2 className="subtitle text-primary">
-                Pool Equipement Installation & Repair
-              </h2>
+              <Link to="/services/repairs" className="text-decoration-none">
+                <h2 className="subtitle text-primary">
+                  Pool Equipement Installation & Repair
+                </h2>
+              </Link>
               <p>
                 FloPool provides leak detection service, repairs, and equipment
                 installations for any type of pool, whether commercial or
@@ -54,16 +63,20 @@ const KeyBenefits = () => {
             </div>
           </Col>
           <Col md={4}>
-            <StaticImage
-              src="../../images/home/key-benefits-2.png"
-              alt="Professional pool service care in Miami, Florida"
-              placeholder="tracedSVG"
-              height={250}
-            />
+            <Link to="/about">
+              <StaticImage
+                src="../../images/home/key-benefits-2.png"
+                alt="Professional pool service care in Miami, Florida"
+                placeholder="tracedSVG"
+                height={250}
+              />
+            </Link>
             <div className="copy">
-              <h2 className="subtitle text-primary">
-                Licensed & Insured Company
-              </h2>
+              <Link to="/about" className="text-decoration-none">
+                <h2 className="subtitle text-primary">
+                  Licensed & Insured Company
+                </h2>
+              </Link>
               <p>
                 Florida Swimming Pools LLC. is fully licensed and insured with
                 over 15 years of industry experience. All our professional
